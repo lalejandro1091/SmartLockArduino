e906991c542763e68d40bd358fa4626c5a80077f
lalejandro1091 <lalejandro1091@hotmail.com>
2016-05-16 02:02:32 +0000

    correcciones en luces

--- aobrero/aobrero.ino
+++ aobrero/aobrero.ino
@@ -6,6 +6,8 @@
 
 
 String codera = "";
+String codera1 = "";
+String codera2 = "";
 int luz1 = 13;
 int luz2 = 12;
 void setup()
@@ -19,8 +21,9 @@ void loop()
     if(Serial.available()>0){ //si esta disponible la lectura serial
       /*array de lectura por string*/
       codera = Serial.readString(); //Guardamos la lectura en una variable String
-    
-    if(codera == "luce1e" || codera == "luce2e"){
+      codera1 = codera.substring(0, 5);
+      codera2 = codera.substring(6);
+    if(codera1 == "luce1e" || codera2 == "luce2e"){
       char nsw = codera[4];
     switch(nsw){
       case '1':
@@ -30,7 +33,7 @@ void loop()
         digitalWrite(luz2, HIGH);
         break;
     }
-  }else if(codera == "luce1a" || codera == "luce2a"){
+  }else if(codera1 == "luce1a" || codera2 == "luce2a"){
       char nsw = codera[4];
       switch(nsw){
       case 1:
